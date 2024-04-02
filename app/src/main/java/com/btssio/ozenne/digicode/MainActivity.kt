a2cd567eadc075325a501c90b3c8fa95ac1e3fcc
medhidev <grimalmedhi4@gmail.com>
2024-04-02 17:05:27 +0200

    login activity + intent redirect login activity

--- app/src/main/java/com/btssio/ozenne/digicode/MainActivity.kt
+++ app/src/main/java/com/btssio/ozenne/digicode/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.btssio.ozenne.digicode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
@@ -10,17 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Variables components XML
         val wifikey_id = findViewById<EditText>(R.id.wifi_key)
         val wifikey = wifikey_id.text.toString();
+        val btn_logout = findViewById<Button>(R.id.btn_logout)
         val qrcode = findViewById<WebView>(R.id.QR_Code)
+        val intent_runLogin = Intent(this, LoginActivity::class.java)
 
-        // Chargez l'URL
+        // URL loading - QRCode
         qrcode.loadUrl("https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$wifikey")
 
         // Display
         val duration = Toast.LENGTH_SHORT
-
         val toast = Toast.makeText(this, wifikey, duration) // in Activity
-        toast.show()
+
+        // Redirection
+        btn_logout.setOnClickListener {
+            startActivity(intent_runLogin) // Login Activity
+        }
     }
 }
